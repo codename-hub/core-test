8cae45f9cd186c146600f67fd9eceb4fb59eec47
Kevin <kevin@jocoon.de>
2021-06-14 16:22:56 +0200

    added overrideableApp::emulateRequest()

--- src/base.php
+++ src/base.php
@@ -108,6 +108,92 @@ abstract class base extends \PHPUnit\Framework\TestCase {
   }
 
   /**
+   * [emulateRequest description]
+   * @param string  $method         [description]
+   * @param string  $host           [description]
+   * @param string  $uri            [description]
+   * @param [type]  $payload        [description]
+   * @param array   $headers        [description]
+   * @param bool    $defaultHeaders [description]
+   */
+  protected static function emulateRequest(string $method, string $host, string $uri, array $get = [], $payload = null, array $headers = [], bool $defaultHeaders = true): void {
+
+    if(!function_exists('getallheaders')) {
+      /**
+       * getallheaders polyfill
+       * for CLI environment, via $_SERVER['HTTP...'] vars
+       * @return array
+       */
+      function getallheaders() {
+        //
+        // From https://github.com/ralouphie/getallheaders/blob/develop/src/getallheaders.php
+        //
+        $headers = array();
+
+        $copy_server = array(
+          'CONTENT_TYPE'   => 'Content-Type',
+          'CONTENT_LENGTH' => 'Content-Length',
+          'CONTENT_MD5'    => 'Content-Md5',
+        );
+
+        foreach ($_SERVER as $key => $value) {
+          if (substr($key, 0, 5) === 'HTTP_') {
+            $key = substr($key, 5);
+            if (!isset($copy_server[$key]) || !isset($_SERVER[$key])) {
+              $key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', $key))));
+              $headers[$key] = $value;
+            }
+          } elseif (isset($copy_server[$key])) {
+            $headers[$copy_server[$key]] = $value;
+          }
+        }
+
+        if (!isset($headers['Authorization'])) {
+          if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+            $headers['Authorization'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+          } elseif (isset($_SERVER['PHP_AUTH_USER'])) {
+            $basic_pass = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
+            $headers['Authorization'] = 'Basic ' . base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $basic_pass);
+          } elseif (isset($_SERVER['PHP_AUTH_DIGEST'])) {
+            $headers['Authorization'] = $_SERVER['PHP_AUTH_DIGEST'];
+          }
+        }
+
+        return $headers;
+      }
+    }
+
+    // TODO: restore previous state?
+    // or done via PHPUnit automatically?
+
+    if(in_array($method, [ 'HEAD', 'OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE' ])) {
+      $_SERVER['REQUEST_METHOD'] = $method;
+    } else {
+      throw new \Exception("Invalid method for emulateRequest: '$method'");
+    }
+    $_SERVER['SERVER_NAME'] = $host;
+    $_SERVER['REQUEST_URI'] = $uri;
+
+    if($defaultHeaders) {
+      // add default headers
+      $headers = array_replace([  ], $headers);
+    }
+
+    // header emulation by $_SERVER['HTTP_...'] ?
+    foreach($headers as $key => $value) {
+      $headerName = strtoupper(str_replace('-', '_', $key));
+      $_SERVER['HTTP_'.$headerName] = $value;
+    }
+
+    foreach($get as $key => $value) {
+      $_GET[$key] = $value;
+    }
+
+    // TODO: payload injection? GET? POST? REQUEST array?
+    // php://input writing?
+  }
+
+  /**
    * models in this environment/test case
    * @var array
    */
